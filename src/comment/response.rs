2813a25378248795d669a4981090249946ed7fa3
Garen Wang <g4r3nwang@gmail.com>
2022-12-16 14:07:32 +0800

    update type of responses

--- src/comment/response.rs
+++ src/comment/response.rs
@@ -1,29 +1,14 @@
-use chrono::NaiveDateTime;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 use super::model::Comment;
 
-
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct CommentResponse {
-    pub comments: Vec<CommentContent>,
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct CommentContent {
-    pub author_id: i32,
-    pub body: String,
-    pub created_at: NaiveDateTime,
+    pub comments: Vec<Comment>,
 }
 
 impl From<Vec<Comment>> for CommentResponse {
     fn from(comments: Vec<Comment>) -> Self {
-        Self {
-            comments: comments.into_iter().map(|x| CommentContent {
-                author_id: x.author_id,
-                body: x.body,
-                created_at: x.created_at,
-            }).collect()
-        }
+        Self { comments }
     }
 }
